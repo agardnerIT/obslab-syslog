80d0d8e9d0f1c3c5f29aa35f1818e5fb01f87c85
Adam Gardner <adam.gardner@dynatrace.com>
2024-08-28 11:21:53 +1000

    add creation ping

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,3 +6,13 @@ COLLECTOR_VERSION=0.12.0
 wget https://github.com/Dynatrace/dynatrace-otel-collector/releases/download/v${COLLECTOR_VERSION}/dynatrace-otel-collector_${COLLECTOR_VERSION}_Linux_x86_64.tar.gz
 tar -xf dynatrace-otel-collector_${COLLECTOR_VERSION}_Linux_x86_64.tar.gz
 rm dynatrace-otel-collector_${COLLECTOR_VERSION}_Linux_x86_64.tar.gz
+
+# Creation Ping
+curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"tenant\": \"$DT_ENDPOINT_SYSLOG\",
+    \"repo\": \"$GITHUB_REPOSITORY\",
+    \"demo\": \"obslab-syslog\",
+    \"codespace.name\": \"$CODESPACE_NAME\"
+  }"
